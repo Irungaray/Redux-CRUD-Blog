bd731f4c302b205276ebf1de16f599e645c922b5
Irungaray <lionelirungaray@gmail.com>
2020-12-02 20:35:15 -0300

    Changed App from Stateless to Stateful

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      Hola putos!
-    </div>
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [
+        {
+          name: 'Capo',
+          mail: 'Capo@delagranputa.com',
+          link: 'capo.com'
+        },
+        {
+          name: 'superCapo',
+          mail: 'superCapo@delagranputa.com',
+          link: 'superCapo.com'
+        }
+      ]
+    }
+  }
+
+  addRows = () => (
+    this.state.users.map((user) => (
+      <tr>
+        <td>
+          { user.name }
+        </td>
+
+        <td>
+          { user.mail }
+        </td>
+
+        <td>
+          { user.link }
+        </td>
+      </tr>
+    ))
   );
-}
+
+  render() {
+    return (
+      <div className="margin">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+
+              <th>Correo</th>
+
+              <th>Enlace</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            { this.addRows() }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+};
 
 export default App;
